63246100a540ebaaa4d70419d9611f059e8e46c0
JC Ryan <jc_ryan@live.com>
2024-11-12 13:55:23 -0500

    initial commit

---
+++ Scripts/Characters/Player/Player.cs
@@ -0,0 +1,32 @@
+using Godot;
+using System;
+
+public partial class Player : CharacterBody3D
+{
+    [ExportGroup("Required Nodes")]
+    [Export] public AnimationPlayer animPlayerNode;
+    [Export] public Sprite3D spriteNode;
+    [Export] public StateMachine stateMachineNode;
+
+    public Vector2 direction = new();
+
+    public override void _Ready() {
+    }
+
+    public override void _PhysicsProcess(double delta) {
+        Velocity = new(direction.X,0,direction.Y);
+        Velocity *= 5;
+        MoveAndSlide();
+        Flip();
+	}
+    public override void _Input( InputEvent @event ) {
+        direction = Input.GetVector(GameConstants.INPUT_MOVE_LEFT , GameConstants.INPUT_MOVE_RIGHT , GameConstants.INPUT_MOVE_FORWARD , GameConstants.INPUT_MOVE_BACKWARD);
+  
+    }
+    private void Flip() {
+        bool isNotMovingHorizontally = Velocity.X == 0;
+        if ( isNotMovingHorizontally ) return;
+        bool isMovingLeft = Velocity.X < 0;
+        spriteNode.FlipH = isMovingLeft;
+    }
+}
---
+++ Scripts/General/GameConstants.cs
@@ -0,0 +1,14 @@
+using Godot;
+
+public class GameConstants {
+
+    //Animation
+    public const string ANIM_IDLE = "Idle";
+    public const string ANIM_MOVE = "Move";
+    
+    //Input
+    public const string INPUT_MOVE_RIGHT = "MoveRight";
+    public const string INPUT_MOVE_LEFT = "MoveLeft";
+    public const string INPUT_MOVE_FORWARD = "MoveForward";
+    public const string INPUT_MOVE_BACKWARD = "MoveBackward";
+}
